85f9a02cda7b22eebdbf3e0f690ecc19ec30c0d4
sysadmin-imran <syedimran.ec@gmail.com>
2020-12-12 18:35:35 +0530

    Spring-boot-app

---
+++ src/test/java/com/szc/dockerspringboot/DockerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szc.dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
